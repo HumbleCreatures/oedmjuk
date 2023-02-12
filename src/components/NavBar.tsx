a8128207bb0a500c211fca57ff9b79974344a0a9
Emil Cardell <emil.cardell@gmail.com>
2023-02-12 23:11:34 +0100

    Adds navigation layout

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,224 @@
+import {
+  createStyles,
+  Navbar,
+  TextInput,
+  Code,
+  UnstyledButton,
+  Badge,
+  Text,
+  Group,
+  ActionIcon,
+  Tooltip,
+} from "@mantine/core";
+import {
+  IconBulb,
+  IconUser,
+  IconCheckbox,
+  IconSearch,
+  IconPlus,
+  IconSelector,
+} from "@tabler/icons";
+import { UserButton } from "./UserButton";
+
+const useStyles = createStyles((theme) => ({
+  navbar: {
+    paddingTop: 0,
+  },
+
+  section: {
+    marginLeft: -theme.spacing.md,
+    marginRight: -theme.spacing.md,
+    marginBottom: theme.spacing.md,
+
+    "&:not(:last-of-type)": {
+      borderBottom: `1px solid ${
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[4]
+          : theme.colors.gray[3]
+      }`,
+    },
+  },
+
+  sectionSpaces: {
+    marginLeft: -theme.spacing.md,
+    marginRight: -theme.spacing.md,
+    marginBottom: theme.spacing.md,
+    flexGrow: 1,
+    flex: 'none',
+  },
+
+  searchCode: {
+    fontWeight: 700,
+    fontSize: 10,
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[7]
+        : theme.colors.gray[0],
+    border: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.colors.gray[2]
+    }`,
+  },
+
+  mainLinks: {
+    paddingLeft: theme.spacing.md - theme.spacing.xs,
+    paddingRight: theme.spacing.md - theme.spacing.xs,
+    paddingBottom: theme.spacing.md,
+  },
+
+  mainLink: {
+    display: "flex",
+    alignItems: "center",
+    width: "100%",
+    fontSize: theme.fontSizes.xs,
+    padding: `8px ${theme.spacing.xs}px`,
+    borderRadius: theme.radius.sm,
+    fontWeight: 500,
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[0]
+        : theme.colors.gray[7],
+
+    "&:hover": {
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+      color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    },
+  },
+
+  mainLinkInner: {
+    display: "flex",
+    alignItems: "center",
+    flex: 1,
+  },
+
+  mainLinkIcon: {
+    marginRight: theme.spacing.sm,
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[2]
+        : theme.colors.gray[6],
+  },
+
+  mainLinkBadge: {
+    padding: 0,
+    width: 20,
+    height: 20,
+    pointerEvents: "none",
+  },
+
+  collections: {
+    paddingLeft: theme.spacing.md - 6,
+    paddingRight: theme.spacing.md - 6,
+    paddingBottom: theme.spacing.md,
+  },
+
+  collectionsHeader: {
+    paddingLeft: theme.spacing.md + 2,
+    paddingRight: theme.spacing.md,
+    marginBottom: 5,
+  },
+
+  footer: {
+    marginLeft: -theme.spacing.md,
+    marginRight: -theme.spacing.md,
+    borderTop: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
+
+  collectionLink: {
+    display: "block",
+    padding: `8px ${theme.spacing.xs}px`,
+    textDecoration: "none",
+    borderRadius: theme.radius.sm,
+    fontSize: theme.fontSizes.xs,
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[0]
+        : theme.colors.gray[7],
+    lineHeight: 1,
+    fontWeight: 500,
+
+    "&:hover": {
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+      color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    },
+  },
+}));
+
+const links = [
+  { icon: IconBulb, label: "Activity", notifications: 3 },
+  { icon: IconCheckbox, label: "Tasks", notifications: 4 },
+  { icon: IconUser, label: "Contacts" },
+];
+
+const collections = [
+  { emoji: "🍱", label: "Space 1" },
+  { emoji: "🍱", label: "Space 2" },
+  { emoji: "🍱", label: "Space 3" },
+  { emoji: "🍱", label: "Space 4" },
+];
+
+export function NavbarSearch() {
+  const { classes } = useStyles();
+
+  const mainLinks = links.map((link) => (
+    <UnstyledButton key={link.label} className={classes.mainLink}>
+      <div className={classes.mainLinkInner}>
+        <link.icon size={20} className={classes.mainLinkIcon} stroke={1.5} />
+        <span>{link.label}</span>
+      </div>
+      {link.notifications && (
+        <Badge size="sm" variant="filled" className={classes.mainLinkBadge}>
+          {link.notifications}
+        </Badge>
+      )}
+    </UnstyledButton>
+  ));
+
+  const collectionLinks = collections.map((collection) => (
+    <div key={collection.label} className={classes.collectionLink}>
+      <span style={{ marginRight: 9, fontSize: 16 }}>{collection.emoji}</span>{" "}
+      {collection.label}
+    </div>
+  ));
+
+  return (
+    <Navbar width={{ sm: 300 }} p="md" className={classes.navbar}>
+      <Navbar.Section className={classes.section}>
+        <div>Oedmjuk - The space to be</div>
+      </Navbar.Section>
+
+      <Navbar.Section className={classes.section}>
+        <div className={classes.mainLinks}>{mainLinks}</div>
+      </Navbar.Section>
+
+      <Navbar.Section className={classes.sectionSpaces} grow={true}>
+        <Group className={classes.collectionsHeader} position="apart">
+          <Text size="xs" weight={500} color="dimmed">
+            Spaces
+          </Text>
+          <Tooltip label="Create space" withArrow position="right">
+            <ActionIcon variant="default" size={18}>
+              <IconPlus size={12} stroke={1.5} />
+            </ActionIcon>
+          </Tooltip>
+        </Group>
+        <div className={classes.collections}>{collectionLinks}</div>
+      </Navbar.Section>
+
+      <Navbar.Section className={classes.footer}>
+        <UserButton
+          image="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
+          name="Ann Nullpointer"
+          email="anullpointer@yahoo.com"
+        />
+      </Navbar.Section>
+    </Navbar>
+  );
+}
