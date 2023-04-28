beb2299575c4d29ca0e7020cf5ef8291758a08c8
Emil Cardell <emil.cardell@gmail.com>
2023-04-29 00:45:49 +0200

    Adds voting results

--- src/pages/app/space/[spaceId]/proposal/[itemId].tsx
+++ src/pages/app/space/[spaceId]/proposal/[itemId].tsx
@@ -24,92 +24,208 @@ function ProposalView({
   const closeObjectionRound = api.proposal.closeObjectionRound.useMutation({
     onSuccess: () => {
       void proposalResult.refetch();
-    }
+    },
+  });
+
+  const endVoting = api.proposal.endVoting.useMutation({
+    onSuccess: () => {
+      void proposalResult.refetch();
+    },
   });
 
   const castVote = api.proposal.castVote.useMutation({
     onSuccess: () => {
       void proposalResult.refetch();
       void voteResult.refetch();
-    }
+    },
   });
 
-  const [voteState, setVoteState] = useState<{ voteValue?:VoteValue, myPickId?: string, myPickChosen: boolean }>({myPickChosen: false});
-  
-  if(proposalResult.isLoading || spaceReslt.isLoading ) return <div>loading...</div>;
+  const [voteState, setVoteState] = useState<{
+    voteValue?: VoteValue;
+    myPickId?: string;
+    myPickChosen: boolean;
+    voteAgain?: boolean;
+  }>({ myPickChosen: false });
+
+  if (proposalResult.isLoading || spaceReslt.isLoading)
+    return <div>loading...</div>;
 
   if (!spaceReslt.data) return <div>Could not load space</div>;
   const { space, isMember } = spaceReslt.data;
 
   if (!proposalResult.data) return <div>Could not load proposal</div>;
-  const { objections, proposalState, title, body, participants } = proposalResult.data;
+  const { proposal, votingResult } = proposalResult.data;
+  if (!proposal) return <div>Could not load proposal</div>;
+  const { objections, proposalState, title, body, participants } = proposal;
 
-
+  if (proposal.proposalState === ProposalStates.VoteClosed) {
+    if (!votingResult) return <div>Could not load voting result</div>;
+    const {
+      numberOfAbstains,
+      numberOfParticipants,
+      numberOfVotes,
+      numberOfAccepts,
+      numberOfRejects,
+      numberOfMissedVotes,
+    } = votingResult;
+    return (
+      <AppLayout>
+        <Container size="xs">
+          <SpaceNavBar space={space} isMember={isMember} />
+          <>
+            <Text fz="lg" fw={500}>
+              <Title order={2}>Proposal: {title}</Title>
+            </Text>
+            <Text>
+              <div dangerouslySetInnerHTML={{ __html: body }} />
+            </Text>
+          </>
+        </Container>
+        <Container size="xs">
+          <div>CLOSED</div>
+          <ListOfObjections objections={objections || []} />
+        </Container>
+        <Container size="xs">
+          Vote information.
+          <div>NumberOfParticipants:{numberOfParticipants}</div>
+          <div>Votes::{numberOfVotes}</div>
+          <div>Accepts:{numberOfAccepts}</div>
+          <div>Rejects:{numberOfRejects}</div>
+          <div>Abstains:{numberOfAbstains}</div>
+          <div>Misses:{numberOfMissedVotes}</div>
+        </Container>
+      </AppLayout>
+    );
+  }
 
   return (
     <AppLayout>
       <Container size="xs">
         <SpaceNavBar space={space} isMember={isMember} />
 
-       
-       
-          <>
-            <Text fz="lg" fw={500}>
-              <Title order={2}>Proposal: {title}</Title>
-            </Text>
-            <Text>
-              <div
-                dangerouslySetInnerHTML={{ __html: body }}
-              />
-            </Text>
-          </>
-        
+        <>
+          <Text fz="lg" fw={500}>
+            <Title order={2}>Proposal: {title}</Title>
+          </Text>
+          <Text>
+            <div dangerouslySetInnerHTML={{ __html: body }} />
+          </Text>
+        </>
       </Container>
       <Container size="xs">
-        {proposalState === ProposalStates.ProposalCreated && <ObjectionEditor proposalId={proposalId} /> }
-        <ListOfObjections objections={objections || []} /> 
-        {objections.filter(o => !o.resolvedAt).length === 0 ? 
-          <Button onClick={() => closeObjectionRound.mutate({proposalId})}>Start voting round</Button> : 
-          <Button disabled={true}>Start voting round</Button>}        
+        {proposalState === ProposalStates.ProposalCreated && (
+          <ObjectionEditor proposalId={proposalId} />
+        )}
+        <ListOfObjections objections={objections || []} />
+        {objections.filter((o) => !o.resolvedAt).length === 0 ? (
+          <Button onClick={() => closeObjectionRound.mutate({ proposalId })}>
+            Start voting round
+          </Button>
+        ) : (
+          <Button disabled={true}>Start voting round</Button>
+        )}
       </Container>
-      
-      {proposalState === ProposalStates.ObjectionsResolved && <Container size="xs">
-        <Text fz="lg" fw={500}> 
-        The voting round is now open.
 
-        {voteResult.data && <div>
-          You have already voted. You voted {voteResult.data.accept === undefined ? "Abstain" : voteResult.data.accept ? "Accept" : "Rejected"} and picked {voteResult.data.myPickId}
-          </div>}
-        <Radio.Group
-      name="acceptOrDeny"
-      label="Vote to accept or deny the proposal"
-      description="Make your voice heard"
-      onChange={(value) => setVoteState({...voteState, voteValue: value as VoteValue})}
-    >
-      <Group mt="xs">
-        <Radio value="Accept" label="Accept"  />
-        <Radio value="Reject" label="Reject" onChange={() => setVoteState({...voteState, voteValue: VoteValue.Reject})}/>
-        <Radio value="Abstain" label="Abstain" onChange={() => setVoteState({...voteState, voteValue: VoteValue.Abstain})} />
-      </Group>
-    </Radio.Group>
+      {proposalState === ProposalStates.ObjectionsResolved && (
+        <Container size="xs">
+          <Text fz="lg" fw={500}>
+            The voting round is now open.
+            {voteResult.data && (
+              <div>
+                You have already voted.
+                <Button
+                  onClick={() =>
+                    setVoteState({ ...voteState, voteAgain: true })
+                  }
+                >
+                  Make new vote
+                </Button>
+              </div>
+            )}
+            {(!voteResult.data || voteState.voteAgain) && (
+              <>
+                <Radio.Group
+                  name="acceptOrDeny"
+                  label="Vote to accept or deny the proposal"
+                  description="Make your voice heard"
+                  onChange={(value) =>
+                    setVoteState({
+                      ...voteState,
+                      voteValue: value as VoteValue,
+                    })
+                  }
+                >
+                  <Group mt="xs">
+                    <Radio value="Accept" label="Accept" />
+                    <Radio
+                      value="Reject"
+                      label="Reject"
+                      onChange={() =>
+                        setVoteState({
+                          ...voteState,
+                          voteValue: VoteValue.Reject,
+                        })
+                      }
+                    />
+                    <Radio
+                      value="Abstain"
+                      label="Abstain"
+                      onChange={() =>
+                        setVoteState({
+                          ...voteState,
+                          voteValue: VoteValue.Abstain,
+                        })
+                      }
+                    />
+                  </Group>
+                </Radio.Group>
 
-    <Radio.Group
-      name=""
-      label="Make your pick"
-      description="Who do you think is an expert on this topic? Who do you trust?"
-      onChange={(value) => setVoteState({...voteState, myPickId: value, myPickChosen: true})} 
-    >
-      <Group mt="xs">
-        {participants.map(p => <UserRadioButton key={p.participantId} userId={p.participantId}  />)}
-        <Radio value="none" label="No one" />
-      </Group>
-    </Radio.Group>
-    <Button disabled={!voteState.voteValue || !voteState.myPickChosen}
-    onClick={() => castVote.mutate({proposalId, voteValue: voteState.voteValue === undefined ? VoteValue.Abstain : voteState.voteValue, myPickId: voteState.myPickId}) }
-    >Vote</Button>
-        </Text>      
-      </Container> }
-
+                <Radio.Group
+                  name=""
+                  label="Make your pick"
+                  description="Who do you think is an expert on this topic? Who do you trust?"
+                  onChange={(value) =>
+                    setVoteState({
+                      ...voteState,
+                      myPickId: value,
+                      myPickChosen: true,
+                    })
+                  }
+                >
+                  <Group mt="xs">
+                    {participants.map((p) => (
+                      <UserRadioButton
+                        key={p.participantId}
+                        userId={p.participantId}
+                      />
+                    ))}
+                    <Radio value="none" label="No one" />
+                  </Group>
+                </Radio.Group>
+                <Button
+                  disabled={!voteState.voteValue || !voteState.myPickChosen}
+                  onClick={() => {
+                    castVote.mutate({
+                      proposalId,
+                      voteValue:
+                        voteState.voteValue === undefined
+                          ? VoteValue.Abstain
+                          : voteState.voteValue,
+                      myPickId: voteState.myPickId,
+                    });
+                    setVoteState({ ...voteState, voteAgain: false });
+                  }}
+                >
+                  Vote
+                </Button>
+              </>
+            )}
+            <Button onClick={() => endVoting.mutate({ proposalId })}>
+              End vote and show results
+            </Button>
+          </Text>
+        </Container>
+      )}
     </AppLayout>
   );
 }
@@ -119,12 +235,14 @@ const ProposalPage: NextPage = () => {
   const { spaceId, itemId } = router.query;
   if (!spaceId || !itemId) return <div>loading...</div>;
 
-  return <ProposalView spaceId={spaceId as string} proposalId={itemId as string} />;
+  return (
+    <ProposalView spaceId={spaceId as string} proposalId={itemId as string} />
+  );
 };
 
-function UserRadioButton ({userId, }: {userId: string}) {
-  const user = api.user.getUser.useQuery({userId}).data;
-  if(!user) return (<div>loading...</div>);
+function UserRadioButton({ userId }: { userId: string }) {
+  const user = api.user.getUser.useQuery({ userId }).data;
+  if (!user) return <div>loading...</div>;
   return <Radio key={userId} value={userId} label={user.name} />;
 }
 
--- src/server/api/routers/proposalRouter.ts
+++ src/server/api/routers/proposalRouter.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
-import { FeedEventTypes, ProposalStates, VoteValue } from "../../../utils/enums";
+import {
+  FeedEventTypes,
+  ProposalStates,
+  VoteValue,
+} from "../../../utils/enums";
 
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { ProposalVote } from "@prisma/client";
 
 export const proposalRouter = createTRPCRouter({
   createProposal: protectedProcedure
@@ -28,7 +33,7 @@ export const proposalRouter = createTRPCRouter({
         },
       });
     }),
-    addObjection: protectedProcedure
+  addObjection: protectedProcedure
     .input(
       z.object({
         body: z.string(),
@@ -44,7 +49,7 @@ export const proposalRouter = createTRPCRouter({
         },
       });
     }),
-    resolveObjection: protectedProcedure
+  resolveObjection: protectedProcedure
     .input(
       z.object({
         comment: z.string(),
@@ -56,13 +61,13 @@ export const proposalRouter = createTRPCRouter({
         where: { id: input.objectionId },
       });
 
-      if(!objection) throw new Error("Objection not found");
+      if (!objection) throw new Error("Objection not found");
 
-      if(objection.resolvedAt) throw new Error("Objection already resolved");
-      
+      if (objection.resolvedAt) throw new Error("Objection already resolved");
+
       return await ctx.prisma.objection.update({
-        where: { 
-          id: input.objectionId
+        where: {
+          id: input.objectionId,
         },
         data: {
           resolvedComment: input.comment,
@@ -71,7 +76,7 @@ export const proposalRouter = createTRPCRouter({
         },
       });
     }),
-    closeObjectionRound: protectedProcedure
+  closeObjectionRound: protectedProcedure
     .input(
       z.object({
         comment: z.string().optional(),
@@ -81,61 +86,97 @@ export const proposalRouter = createTRPCRouter({
     .mutation(async ({ ctx, input }) => {
       const proposal = await ctx.prisma.proposal.findUnique({
         where: { id: input.proposalId },
-        include: { objections: true }
+        include: { objections: true },
       });
 
-      if(!proposal) throw new Error("Objection not found");
+      if (!proposal) throw new Error("Objection not found");
 
-      const openObjections = proposal.objections.filter(o => !o.resolvedAt);
-      if(openObjections.length > 0) throw new Error("There are still open objections");
+      const openObjections = proposal.objections.filter((o) => !o.resolvedAt);
+      if (openObjections.length > 0)
+        throw new Error("There are still open objections");
 
       const allSpaceUsers = await ctx.prisma.spaceMember.findMany({
         where: {
           spaceId: proposal.spaceId,
-        }
-       });
+        },
+      });
 
       return await ctx.prisma.proposal.update({
-        where: { 
-          id: input.proposalId
+        where: {
+          id: input.proposalId,
         },
         data: {
           proposalState: ProposalStates.ObjectionsResolved,
           participants: {
-            create: allSpaceUsers.map(u => ({ participantId: u.userId }))
-          }
+            create: allSpaceUsers.map((u) => ({ participantId: u.userId })),
+          },
         },
       });
     }),
-    castVote: protectedProcedure
+  castVote: protectedProcedure
     .input(
       z.object({
         proposalId: z.string(),
-        voteValue: z.enum([VoteValue.Accept, VoteValue.Reject, VoteValue.Abstain]),
+        voteValue: z.enum([
+          VoteValue.Accept,
+          VoteValue.Reject,
+          VoteValue.Abstain,
+        ]),
         myPickId: z.string().optional(),
       })
     )
     .mutation(async ({ ctx, input }) => {
       const proposal = await ctx.prisma.proposal.findUnique({
         where: { id: input.proposalId },
-        include: { participants: true }
+        include: { participants: true },
       });
 
-      if(!proposal) throw new Error("Objection not found");
+      if (!proposal) throw new Error("Objection not found");
 
-      if(proposal.proposalState !== ProposalStates.ObjectionsResolved) throw new Error("Proposal not in votable state");
-      if(input.myPickId && !proposal.participants.some(p => p.participantId === input.myPickId)) throw new Error("Your myPick is invalid.");
-      
+      if (proposal.proposalState !== ProposalStates.ObjectionsResolved)
+        throw new Error("Proposal not in votable state");
+      if (
+        input.myPickId &&
+        !proposal.participants.some((p) => p.participantId === input.myPickId)
+      )
+        throw new Error("Your myPick is invalid.");
+
       return await ctx.prisma.proposalVote.create({
         data: {
           proposalId: input.proposalId,
           myPickId: input.myPickId,
           userId: ctx.session.user.id,
-          accept: input.voteValue === VoteValue.Abstain ? undefined : input.voteValue === VoteValue.Accept,
-        }
+          accept:
+            input.voteValue === VoteValue.Abstain
+              ? undefined
+              : input.voteValue === VoteValue.Accept,
+        },
       });
     }),
-    getUserVote: protectedProcedure
+  endVoting: protectedProcedure
+    .input(
+      z.object({
+        proposalId: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const proposal = await ctx.prisma.proposal.findUnique({
+        where: { id: input.proposalId },
+        include: { participants: true },
+      });
+
+      if (!proposal) throw new Error("Proposal not found");
+
+      return await ctx.prisma.proposal.update({
+        where: {
+          id: input.proposalId,
+        },
+        data: {
+          proposalState: ProposalStates.VoteClosed,
+        },
+      });
+    }),
+  getUserVote: protectedProcedure
     .input(z.object({ proposalId: z.string() }))
     .query(async ({ ctx, input }) => {
       const result = await ctx.prisma.proposalVote.findMany({
@@ -145,24 +186,61 @@ export const proposalRouter = createTRPCRouter({
 
       return result[0];
     }),
-    getSpaceProposals: protectedProcedure
+  getSpaceProposals: protectedProcedure
     .input(z.object({ spaceId: z.string() }))
     .query(async ({ ctx, input }) => {
       return await ctx.prisma.proposal.findMany({
         where: { spaceId: input.spaceId },
         include: {
-          objections: true
-        }
+          objections: true,
+        },
       });
-
     }),
   getProposal: protectedProcedure
     .input(z.object({ proposalId: z.string() }))
     .query(async ({ ctx, input }) => {
-      return await ctx.prisma.proposal.findUnique({
+      const proposal = await ctx.prisma.proposal.findUnique({
         where: { id: input.proposalId },
-        include: { objections: true, participants:true }
+        include: { objections: true, participants: true },
       });
-    }),
 
+      if (!proposal) throw new Error("Proposal not found");
+
+      if (proposal.proposalState === ProposalStates.VoteClosed) {
+        const votes = await ctx.prisma.proposalVote.findMany({
+          where: { proposalId: proposal.id },
+        });
+
+        const uniqeVotes = votes.reduce((acc, vote) => {
+          if (!acc.has(vote.userId)) {
+            acc.set(vote.userId, vote);
+          } else {
+            const recordedVote = acc.get(vote.userId);
+            if (recordedVote && recordedVote.createdAt < vote.createdAt) {
+              acc.set(vote.userId, vote);
+            }
+          }
+          return acc;
+        }, new Map<string, ProposalVote>());
+
+        const voteArray = Array.from(uniqeVotes.values());
+        //TODO: Calculate accept results in %
+        //TODO: Calculate myPick results
+
+        const votingResult = {
+          numberOfParticipants: proposal.participants.length,
+          numberOfVotes: uniqeVotes.size,
+          numberOfAccepts: voteArray.filter((v) => v.accept).length,
+          numberOfRejects: voteArray.filter((v) => !v.accept).length,
+          numberOfAbstains: voteArray.filter((v) => v.accept === undefined).length,
+          numberOfMissedVotes: (proposal.participants.length - uniqeVotes.size),
+        };
+
+        return {
+          proposal,
+          votingResult,
+        };
+      }
+      return { proposal, votingResult: undefined };
+    }),
 });
