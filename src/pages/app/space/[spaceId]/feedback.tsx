8ddacb0193f994c17a43450cd9ed222835d9ff43
Emil Cardell <emil.cardell@gmail.com>
2023-08-06 23:23:27 +0200

    Fixed feedback list

--- src/pages/app/space/[spaceId]/feedback.tsx
+++ src/pages/app/space/[spaceId]/feedback.tsx
@@ -1,12 +1,18 @@
 import { useRouter } from 'next/router'
 import { type NextPage } from "next";
 import AppLayout from "../../../../components/AppLayout";
-import { Container, Title, SimpleGrid, createStyles, Card, Text } from "@mantine/core";
+import { Container, Title, SimpleGrid, createStyles, Card, Text, Group, Tabs, Badge } from "@mantine/core";
 import { api } from "../../../../utils/api";
 import { SpaceNavBar } from '../../../../components/SpaceNavBar';
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { DateTime } from "luxon";
 import Link from 'next/link';
+import { useGeneralStyles } from '../../../../styles/generalStyles';
+import { SpaceLoader } from '../../../../components/Loaders/SpaceLoader';
+import { FeedbackRoundStates } from '../../../../utils/enums';
+import { IconRecycle } from '@tabler/icons';
+import { FeedbackRound } from '@prisma/client';
+import { FeedbackRoundStatusBadge } from '../../../../components/FeedbackRoundStatusBadge';
 
 
 
@@ -23,56 +29,173 @@ const useStyles = createStyles((theme) => ({
   },
   inlineText: {
     display: "inline",
+  },
+  titleWrapper: {
+    marginBottom: theme.spacing.md,
+  },
+  title: {
+    fontSize: theme.fontSizes.md,
   }
 }));
 
 function SpaceView({spaceId}: {spaceId: string}) {
   const { classes } = useStyles();
+  const { classes: generalClasses } = useGeneralStyles(); 
   
 
-  const spaceRequest = api.space.getSpace.useQuery({spaceId});
-  const feedbackRequest = api.feedback.getFeedbackRoundsForSpace.useQuery({spaceId});
-  if(spaceRequest.isLoading || feedbackRequest.isLoading) return (<div>loading...</div>);
-  if(spaceRequest.error || feedbackRequest.error) return (<div>error</div>);
-  if(spaceRequest.data === undefined || feedbackRequest.data === undefined) return (<div>not found</div>);
-  const {space, isMember} = spaceRequest.data;
+  const spaceResult = api.space.getSpace.useQuery({spaceId});
+  const feedbackResult = api.feedback.getFeedbackRoundsForSpace.useQuery({spaceId});
+  if (spaceResult.isLoading) return <SpaceLoader />;
+  if (!spaceResult.data)
+    return <div>Could not find space</div>;
+  const { space, isMember } = spaceResult.data;
+  if (feedbackResult.isLoading) return <SpaceLoader space={space} isMember={isMember} />;
+  if (!feedbackResult.data)
+    return <div>Could not find feedback</div>;
+
+  const createdFeedback = feedbackResult.data.filter((selection) => selection.state === FeedbackRoundStates.Created);
+  const openFeedback = feedbackResult.data.filter((selection) => selection.state === FeedbackRoundStates.Started);
+  const closedFeedback = feedbackResult.data.filter((selection) => selection.state === FeedbackRoundStates.Closed);
+
 
   return (
     <AppLayout>
       <Container size="sm">
       <SpaceNavBar space={space} isMember={isMember}/>
-      <div className={classes.area}>
-        <Title order={2} className={classes.areaTitle}>Feedback</Title>
-        <SimpleGrid cols={1}>
-            {feedbackRequest.data.map((feedbackRound) => (<Link href={`/app/space/${feedbackRound.spaceId}/feedback/${feedbackRound.id}`} key={feedbackRound.id}><Card>
-            <Text fz="lg" fw={500}>{feedbackRound.title}</Text>
-            <div>
-                <Text fz="sm" fw={300}>
-                    State:  <Text fz="sm" fw={500} className={classes.inlineText}>{feedbackRound.state}</Text>
-                  </Text>
-                  <Text fz="sm" fw={300}>
-                    Created  <Text fz="sm" fw={500} className={classes.inlineText}>{DateTime.fromJSDate(feedbackRound.createdAt)
-                      .setLocale("en")
-                      .toRelative()}</Text>
-                  </Text>
+      
+      <Container size="sm" className={generalClasses.clearArea}>
+          <Group className={classes.titleWrapper}>
+          <IconRecycle />
+            <Title order={2} className={classes.title}>
+              Feedback rounds
+            </Title>
+          </Group>
+      </Container>
+      <Tabs defaultValue="open" 
+          styles={(theme) => ({
+            tabsList: {
+              border: 'none',
+            },
+            tab: { 
+              color: theme.white,
+              marginBottom: theme.spacing.xs,
+              borderColor: theme.colors.earth[2],
+              borderRadius: 0,
+              '&[right-section]': {
+                backgroundColor: theme.fn.rgba(theme.white, 0.1),
+               },
+              '&:hover': {
+                backgroundColor: theme.fn.rgba(theme.white, 0.05),
+                borderColor: theme.colors.earth[2],
+              },
+              '&[data-active]': {
+                backgroundColor: theme.fn.rgba(theme.white, 0.1),
+                borderColor: theme.white,
+                color: theme.white,
+              },
+              '&[data-active]:hover': {
+                backgroundColor: theme.fn.rgba(theme.white, 0.1),
+                borderColor: theme.white,
+                color: theme.white,
+              },
+            }
+          })}>
+      <Tabs.List grow>
+        <Tabs.Tab
+          rightSection={
+            <Badge
+              w={16}
+              h={16}
+              sx={{ pointerEvents: 'none' }}
+              size="xs"
+              p={0}
+              
+            >
+              {openFeedback.length}
+            </Badge>
+          }
+          value="open"
+        >
+          Open
+        </Tabs.Tab>
+        <Tabs.Tab value="created"
+        rightSection={
+          <Badge
+            w={16}
+            h={16}
+            sx={{ pointerEvents: 'none' }}
+            size="xs"
+            p={0}
+            
+          >
+            {createdFeedback.length}
+          </Badge>
+        }>Created</Tabs.Tab>
+        <Tabs.Tab value="closed"
+        rightSection={
+          <Badge
+            w={16}
+            h={16}
+            sx={{ pointerEvents: 'none' }}
+            size="xs"
+            p={0}
+            
+          >
+            {closedFeedback.length}
+          </Badge>
+        }>Closed</Tabs.Tab>
 
-                  {feedbackRound.updatedAt && feedbackRound.updatedAt.getTime() !== feedbackRound.createdAt.getTime() && <Text fz="sm" fw={300}>
-                    Last updated  <Text fz="sm" fw={500} className={classes.inlineText}>{DateTime.fromJSDate(feedbackRound.updatedAt)
-                      .setLocale("en")
-                      .toRelative()}</Text>
-                  </Text>}
+      </Tabs.List>
+      <Tabs.Panel value="open">
+       <FeedbackList feedbackRound={openFeedback} />
+      </Tabs.Panel>
+      <Tabs.Panel value="created">
+      <FeedbackList feedbackRound={createdFeedback} />
+      </Tabs.Panel>
+      <Tabs.Panel value="closed">
+      <FeedbackList feedbackRound={closedFeedback} />
+      </Tabs.Panel>
 
-                </div>
-
-            </Card></Link>))}
-        </SimpleGrid>
-
-
-      </div>
+    </Tabs>
       </Container>
     </AppLayout>
   );
-} 
+}
+
+function FeedbackList ({feedbackRound}: {feedbackRound: FeedbackRound[]}) { 
+  const { classes } = useStyles();
+  const { classes: generalClasses } = useGeneralStyles(); 
+
+  return (<SimpleGrid cols={1}>
+    {feedbackRound.map((feedback) => (<Link 
+    className={generalClasses.listLinkItem}
+    href={`/app/space/${feedback.spaceId}/feedback/${feedback.id}`} key={feedback.id}><Card>
+    <Group position="apart">
+            <Text fz="lg" fw={500}>
+              {feedback.title}
+            </Text>
+            <FeedbackRoundStatusBadge state={feedback.state} />
+          </Group>
+    <div>
+
+          <Text fz="sm" fw={300}>
+            Created  <Text fz="sm" fw={500} className={classes.inlineText}>{DateTime.fromJSDate(feedback.createdAt)
+              .setLocale("en")
+              .toRelative()}</Text>
+          </Text>
+
+          {feedback.updatedAt && feedback.updatedAt.getTime() !== feedback.createdAt.getTime() && <Text fz="sm" fw={300}>
+            Last updated  <Text fz="sm" fw={500} className={classes.inlineText}>{DateTime.fromJSDate(feedback.updatedAt)
+              .setLocale("en")
+              .toRelative()}</Text>
+          </Text>}
+
+        </div>
+
+    </Card></Link>))}
+</SimpleGrid>)
+}
+
 const LoadingSpaceView: NextPage = () => {
   const router = useRouter()
   
